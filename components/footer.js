35df64569688cad9b604888fba4179147be5aa1c
Ryan Sethi <ryan@ryans-mbp.lan>
2023-01-11 15:55:10 +0000

    footer css

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,23 @@
 import React from "react"
+import styles from "../styles/Footer.module.css"
+
 
 export default function footer() {
-    return <h3>This is the footer.</h3>
+    return <footer>
+
+        <h3 className={styles.footerMain}>Copyright @2022 <span className={styles.footerSpan}> React McReactface</span></h3>
+        <a
+            data-testid="react-link"
+            target="_blank"
+            rel="noreferrer"
+            href="https://reactjs.org/"
+        >
+            <img
+                data-testid="logo-img"
+                className={styles.links}
+                src="/Pictures/react-logo.png"
+                alt="react"
+            />
+        </a>
+    </footer>
 }
