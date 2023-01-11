b47883c390861946bfec881c21ed75afd401e7ff
Tirsog <tirsog@gmail.com>
2023-01-11 11:34:42 +0000

    Navbar and Navbar style

--- components/navbar.js
+++ components/navbar.js
@@ -2,24 +2,20 @@ import Link from "next/link"
 import { useState } from "react"
 import Image from "next/image"
 import { useSession } from "@supabase/auth-helpers-react"
+import styles from "../styles/Navbar.module.css"
 
-function Navbar() {
+export default function Navbar() {
     const [isNavExpanded, setIsNavExpanded] = useState(false)
     const session = useSession()
     return (
-        <nav>
+        <nav className={styles.nav}>
             <Link href="/">
-                <Image
-                    src="vercel.svg"
-                    alt="Logo"
-                    width={137}
-                    height={50}
-                />
+                <Image src="/musoLogo.png" alt="Logo" width={75} height={75} />
             </Link>
-            <div id="mobile">
-                <div className={isNavExpanded ? "hide" : "display"}>
+            <div>
+                <div className={isNavExpanded ? styles.hide : styles.display}>
                     <button
-                        className="icon"
+                        className={styles.icon}
                         onClick={() => {
                             setIsNavExpanded(!isNavExpanded)
                         }}
@@ -42,9 +38,9 @@ function Navbar() {
                         </svg>
                     </button>
                 </div>
-                <div className={isNavExpanded ? "display" : "hide"}>
+                <div className={isNavExpanded ? styles.display : styles.hide}>
                     <button
-                        className="icon"
+                        className={styles.icon}
                         onClick={() => {
                             setIsNavExpanded(!isNavExpanded)
                         }}
@@ -64,27 +60,31 @@ function Navbar() {
                     </button>
                 </div>
             </div>
-            <ul className={isNavExpanded ? "display" : "hide"}>
-                <li>
+            <ul
+                className={`${styles.ul} ${
+                    isNavExpanded ? styles.display : styles.hide
+                }`}
+            >
+                <li className={styles.li}>
                     <Link href={"/"}>Home</Link>
                 </li>
-                <li>
+                <li className={styles.li}>
                     <Link href={"/gigs"}>Gigs</Link>
                 </li>
-                <li>
+                <li className={styles.li}>
                     <Link href={"/musicians"}>Articles</Link>
                 </li>
-                <li>
+                <li className={styles.li}>
                     <Link href={"/contact"}>Contact</Link>
                 </li>
-                <li>
-                {!session ? (
-                    <Link href={"/login"}>Login</Link>
-                    ) : (<Link href={"/login"}>Account</Link>)}
+                <li className={styles.li}>
+                    {!session ? (
+                        <Link href={"/login"}>Login</Link>
+                    ) : (
+                        <Link href={"/login"}>Account</Link>
+                    )}
                 </li>
             </ul>
         </nav>
     )
 }
-
-export default Navbar
