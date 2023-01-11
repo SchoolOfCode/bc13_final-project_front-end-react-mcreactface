fd089020de52759b6bb6557c0f06f93627f1a9d6
Tirsog <tirsog@gmail.com>
2023-01-11 09:55:20 +0000

    fixed component import

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,22 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import navbar from "@/components/navbar"
-import GigsDisplay from '../components/GigsDisplay'
+import Head from "next/head"
+import Image from "next/image"
+import Link from "next/link"
+import GigsDisplay from "../components/GigsDisplay"
 
-import styles from '@/pages/index.module.css'
-import Navbar from '@/components/navbar'
+import styles from "/pages/index.module.css"
+import Navbar from "/components/navbar"
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Welcome to MusoFind</title>
-        <link rel="icon" href="/musoLogo.png" />
-      </Head>
-      <Navbar />
-      <main>
-      <GigsDisplay />
-      </main>
-    </div>
-  )
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Welcome to MusoFind</title>
+                <link rel="icon" href="/musoLogo.png" />
+            </Head>
+            <Navbar />
+            <main>
+                <GigsDisplay />
+            </main>
+        </div>
+    )
 }
