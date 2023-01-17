69b00eb77b0b23a2d9c0c4f5cc17cc1729d31adc
jamiestungo <jamiestungo@gmail.com>
2023-01-17 15:12:25 +0000

    Database query based on multiple genres not functioning as expected

--- components/GigsDisplay.js
+++ components/GigsDisplay.js
@@ -20,10 +20,25 @@ export default function GigsDisplay() {
     const [userData, setUserData] = useState(null)
     const [loading, setLoading] = useState(true)
     const [monthPageOne, setMonthPageOne] = useState(true)
+    const [searchCurrentYear, setSearchCurrentYear] = useState()
+    const [searchInstruments, setSearchInstruments] = useState([])
+    const [searchGenres, setSearchGenres] = useState([])
     const router = useRouter()
     const data = router.query
-
-
+    const moy = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    ]
 
     useEffect(() => {
         supabase.auth.getUser().then((response) => {
@@ -32,24 +47,15 @@ export default function GigsDisplay() {
             setLoading(false)
         })
 
+        let theYear = new Date().getFullYear()
+        if (!searchCurrentYear) {
+            setSearchCurrentYear(theYear)
+        }
+
         getGigs(userData)
     }, [user])
 
     async function getGigs(userData) {
-        console.log("in getGigs, userdata: ", userData)
-        // get the userID ✅
-        // get the genres from the profiles table based on the userID and call it userGenres ✅
-        // get the gigs ✅
-        // filter the gigs based on whether genres matches userGenres (optional show all button)
-        // filter the gigs to remove any gig where gig.id matches userID (optional show my gigs with css styling)
-
-        /* this has to be defined outside of the next block */
-        let { data: gigs, gigsTableError } = await supabase
-            .from("gigs")
-            .select("*")
-
-        console.log("user: ", user)
-
         if (user) {
             /* this has to be defined within the if(user) block otherwise the component can't render */
             let { data: profileTable, profileTableError } = await supabase
@@ -58,31 +64,65 @@ export default function GigsDisplay() {
                 .eq("id", user.id)
                 .single()
 
+            const splitGenres = profileTable.genres[0].split(",")
+            console.log("splitGenres: ", splitGenres)
+
+          /* this is not a very good way to do it but I did it anyway :( - J */
+          let gigs = [];
+
+            for (let thisGenre of splitGenres) {
+              let { data: gigsdata, gigsTableError } = await supabase
+                .from("gigs")
+                .select("*")
+                .gte("starttime", `${searchCurrentYear}-01-01 00:00:00`)
+                .lte("starttime", `${searchCurrentYear}-12-12 23:59:59`)
+                .contains("genres", [thisGenre])
+
+                
+                gigs.push(...gigs, ...gigsdata)
+                console.log("gigs: ", gigs)
+            }
+/*
+            let { data: gigs, gigsTableError } = await supabase
+                .from("gigs")
+                .select("*")
+                .gte("starttime", `${searchCurrentYear}-01-01 00:00:00`)
+                .lte("starttime", `${searchCurrentYear}-12-12 23:59:59`)
+                .contains("genres", splitGenres)
+                //.in  .contains("genres", [splitGenres[0] || splitGenres[1]])
+
+                .order("starttime", { ascending: true })
+*/
+            for (let eachGig of gigs) {
+                let newDate = new Date(eachGig.starttime)
+                eachGig.startday = newDate.getDate()
+                eachGig.startmonth = newDate.getMonth()
+                eachGig.startyear = newDate.getFullYear()
+            }
+
             setOutput(gigs)
             setGenres(profileTable.genres)
-            console.log("profileTable.instruments: ", profileTable.instruments)
 
             setTableState({
                 genres: profileTable.genres,
                 instruments: profileTable.instruments,
             })
-            console.log("Pre-filtered", gigs)
-            if (profileTable.genres) {
+            /*
+            if (genres.length != 0) {
                 gigs = gigs.filter((gig) => {
                     //console.log("FILTERING: gig.genres: ",gig.genres," + profileTable.genres: ",profileTable.genres)
                     return gig.genres.some(
                         (r) => profileTable.genres.indexOf(r) >= 0
                     )
                 })
-            }
+                setOutput(gigs)
+            }*/
         } else {
-            setOutput(gigs)
+            //setOutput(gigs)
         }
     }
     if (loading) return <p>Loading...</p>
     //   if (!userData) return <NoSessionWarn />
-
-    let theYear = new Date().getFullYear()
 
     return (
         <>
@@ -94,7 +134,7 @@ export default function GigsDisplay() {
                         {tableState.instruments ? (
                             <h3>
                                 {"Showing Instruments: " +
-                                    <button>{tableState.instruments}</button>}
+                                <button>{tableState.instruments}</button>}
                             </h3>
                         ) : (
                             "All Instruments"
@@ -104,47 +144,158 @@ export default function GigsDisplay() {
                     "No filters applied"
                 )}
             </h3>
-
-            <div className={styles.ylarrow}>⬅️</div><div className={styles.year}>{theYear}</div><div className={styles.yrarrow}>➡️</div>
-            <div>
-                {monthPageOne ?
-                    <div className={styles.monthParent}>
-
-                        <div className={styles.longMonthBox}>
-                            <div className={styles.month}>Jan</div>
-                            <div className={styles.upArrow}>⬆️</div>
-                            <div className={styles.gigDates}>Item One</div>
-                            <div className={styles.gigDates}>Item Two</div>
-                            <div className={styles.gigDates}>Item Three</div>
-                            <div className={styles.gigDates}>Item Four</div>
-                            <div className={styles.downArrow}>⬇️</div>
-                        </div>
-                        <div className={styles.longMonthBox}><div className={styles.month}>Feb</div><div className={styles.uparrow}>⬆️</div><div className={styles.gigDates}><div className={styles.downarrow}>⬇️</div></div></div>
-                        <div className={styles.longMonthBox}><div className={styles.month}>Mar</div><div className={styles.uparrow}>⬆️</div><div className={styles.gigDates}><div className={styles.downarrow}>⬇️</div></div></div>
-                        <div className={styles.longMonthBox}><div className={styles.month}>Apr</div><div className={styles.uparrow}>⬆️</div><div className={styles.gigDates}><div className={styles.downarrow}>⬇️</div></div></div>
-                        <div className={styles.longMonthBox}><div className={styles.month}>May</div><div className={styles.uparrow}>⬆️</div><div className={styles.gigDates}><div className={styles.downarrow}>⬇️</div></div></div>
-                        <div className={styles.longMonthBox}><div className={styles.month}>Jun</div><div className={styles.uparrow}>⬆️</div><div className={styles.gigDates}><div className={styles.downarrow}>⬇️</div></div></div>
-                    </div> :
-                    <div className={styles.monthParent}><div className={styles.longMonthBox}><div className={styles.month}>Jul</div><div className={styles.uparrow}>⬆️</div><div className={styles.gigDates}><div className={styles.downarrow}>⬇️</div></div></div>
-                        <div className={styles.longMonthBox}><div className={styles.month}>Aug</div><div className={styles.uparrow}>⬆️</div><div className={styles.gigDates}><div className={styles.downarrow}>⬇️</div></div></div>
-                        <div className={styles.longMonthBox}><div className={styles.month}>Sep</div><div className={styles.uparrow}>⬆️</div><div className={styles.gigDates}><div className={styles.downarrow}>⬇️</div></div></div>
-                        <div className={styles.longMonthBox}><div className={styles.month}>Oct</div><div className={styles.uparrow}>⬆️</div><div className={styles.gigDates}><div className={styles.downarrow}>⬇️</div></div></div>
-                        <div className={styles.longMonthBox}><div className={styles.month}>Nov</div><div className={styles.uparrow}>⬆️</div><div className={styles.gigDates}><div className={styles.downarrow}>⬇️</div></div></div>
-                        <div className={styles.longMonthBox}><div className={styles.month}>Dec</div><div className={styles.uparrow}>⬆️</div><div className={styles.gigDates}><div className={styles.downarrow}>⬇️</div></div></div>
-                    </div>
-                }
+            <div className={styles.ycomponents}>
+                <div
+                    className={styles.yarrow}
+                    onClick={() => setSearchCurrentYear(searchCurrentYear - 1)}
+                >
+                    ⬅️
+                </div>
+                <div className={styles.year}>{searchCurrentYear}</div>
+                <div
+                    className={styles.yarrow}
+                    onClick={() => setSearchCurrentYear(searchCurrentYear + 1)}
+                >
+                    ➡️
+                </div>
             </div>
-            <div className={styles.gigParent}>
-                {output.map((gig) => (
-                    <GigItem key={gig.id} gig={gig}></GigItem>
-                ))}
+            <div className={styles.monthParent}>
+                <div
+                    onClick={() => {
+                        setMonthPageOne(true)
+                    }}
+                >
+                    ⬅️
+                </div>
+                {monthPageOne
+                    ? moy.map((themonth, index) => {
+                          if (index < 6) {
+                              return (
+                                  <div className={styles.longMonthBox}>
+                                      <div className={styles.month}>
+                                          {themonth}
+                                      </div>
+                                      <div className={styles.upArrow}>⬆️</div>
+                                      {output
+                                          ? output
+                                                .filter(
+                                                    (gig) =>
+                                                        gig.startmonth ==
+                                                            index &&
+                                                        gig.startyear ==
+                                                            searchCurrentYear
+                                                )
+                                                .map((gig) => {
+                                                    return (
+                                                        <div
+                                                            className={
+                                                                styles.gigDates
+                                                            }
+                                                        >
+                                                            <div
+                                                                className={
+                                                                    styles.gigDay
+                                                                }
+                                                            >
+                                                                {gig.startday}
+                                                            </div>
+                                                            <div
+                                                                className={
+                                                                    styles.gigType
+                                                                }
+                                                            >
+                                                                {gig.eventtype}
+                                                            </div>
+                                                            <div
+                                                                className={
+                                                                    styles.gigPostCode
+                                                                }
+                                                            >
+                                                                {gig.postcode}
+                                                            </div>
+                                                        </div>
+                                                    )
+                                                })
+                                          : ""}
+                                      <div className={styles.downArrow}>⬇️</div>
+                                  </div>
+                              )
+                          }
+                      })
+                    : moy.map((themonth, index) => {
+                          if (index >= 6) {
+                              return (
+                                  <div className={styles.longMonthBox}>
+                                      <div className={styles.month}>
+                                          {themonth}
+                                      </div>
+                                      <div className={styles.upArrow}>⬆️</div>
+                                      {output
+                                          ? output
+                                                .filter(
+                                                    (gig) =>
+                                                        gig.startmonth ==
+                                                            index &&
+                                                        gig.startyear ==
+                                                            searchCurrentYear
+                                                )
+                                                .map((gig) => {
+                                                    return (
+                                                        <div
+                                                            className={
+                                                                styles.gigDates
+                                                            }
+                                                        >
+                                                            <div
+                                                                className={
+                                                                    styles.gigDay
+                                                                }
+                                                            >
+                                                                {gig.startday}
+                                                            </div>
+                                                            <div
+                                                                className={
+                                                                    styles.gigType
+                                                                }
+                                                            >
+                                                                {gig.eventtype}
+                                                            </div>
+                                                            <div
+                                                                className={
+                                                                    styles.gigPostCode
+                                                                }
+                                                            >
+                                                                {gig.postcode}
+                                                            </div>
+                                                        </div>
+                                                    )
+                                                })
+                                          : ""}
+                                      <div className={styles.downArrow}>⬇️</div>
+                                  </div>
+                              )
+                          }
+                      })}
+                <div
+                    onClick={() => {
+                        setMonthPageOne(false)
+                    }}
+                >
+                    ➡️
+                </div>
             </div>
-            {
+            {/*
+                <div className={styles.gigParent}>
+                    {output.map((gig) => (
+                        <GigItem key={gig.id} gig={gig}></GigItem>
+                    ))}
+                </div>
+            */}
+            {/*
                 <h2 className={styles.itemsfooter}>
                     {output.length ? output.length + " Items" : ""}
                 </h2>
-            }
-
+            */}
         </>
     )
 }
