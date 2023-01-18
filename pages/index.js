32d151df481b3d7144966d9d09ce3062806bd119
omarcodes98 <omarali_15@hotmail.co.uk>
2023-01-18 16:23:16 +0000

    homepage layout and personal profiles changes

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
                 <title>Welcome to MusoFind</title>
                 <link rel="icon" href="/musoLogo.png" />
             </Head>
-            <main>
+            <div className={styles.scrolling}>
                 <div className={styles.minipage}>
                     <img
                         className={styles.largeLogo}
@@ -30,45 +30,49 @@ export default function Home() {
                         that are looking for a band or just want to jam.
                     </p>
                 </div>
-                <div className={styles.minipage}>
-                <h2>Booking made easy</h2>
+                <div className={styles.minipage2}>
+                <h2>Management made easy</h2>
+                <div className={styles.sideby}>
+                <div className={styles.sidetext}>
                     <p>
-                        Simply decide on a gig to play at and book your spot.
-                        MusoFind will handle the rest. You can even add your
-                        own gigs to the site. Just make sure to include all the
-                        details so that other musicians can find you.
+                        Looking for a gig? Book your gig with ease. MusoFind has
+                        a simple booking system that allows you to book gigs
+                        with just a few clicks. No more hassle of emailing
+                        back and forth. Just book your gig and get to playing.
+                        The My Gigs page allows you to see all the gigs you have
+                        booked and created. You can even edit or delete your gigs
+                        from this page. All with sleek and simple interface that
+                        makes it easy to use. Have complete control of your gigs
+                        and your schedule at your fingertips.
                     </p>
+                    </div>
+                    <div className={styles.sideimage}>
+                        <img
+                            className={styles.gigImage}
+                            src="gigImage.png"
+                            alt="Gig image"
+                        >
+                        </img>
+                    </div>
+                    </div>
                 </div>
-                <div className={styles.minipage}>
-                <h2>Manage your gigs</h2>
+                <div className={styles.minipage3}>
+                <h2></h2>
+                <div className={styles.sideby}>
+                <div className={styles.sideimage}>
+                    </div>
+                <div className={styles.sidetext2}>
+                <h2>JOIN THE COMMUNTITY</h2>
                 <p>
-                    MusoFind has a dedicated my gigs page where you can see all
-                    the gigs you have booked and created. You can even edit or delete your gigs from
-                    this page. All with sleek and simple interface that makes
-                    it easy to use. Have complete control of your gigs and your
-                    schedule at your fingertips.
-                </p>
-                </div>
-                <div className={styles.minipage}>
-                    <h2>Find Musicians</h2>
-                    <p>
-                        Since MusoFind is a community of musicians, you can also
-                        find other musicians to jam with. You can search for
-                        musicians by instrument, skill level, and style. You can
-                        even search for musicians in your area. Create your own profile and 
-                        add your own instruments and skill level. This way, other musicians 
-                        can find you and you can find them.
-                    </p>
-                </div>
-                <div className={styles.minipage}>
-                    <h2>Join the community</h2>
-                    <p>
                         Making an account is easy and free. You can even sign up with third party
                         services like Google or Facebook. MusoFind is a community of musicians for
-                        musicians. Sign up today and get started.
+                        musicians. Sign up today and get started!
                     </p>
+                    </div>
+                    
+                    </div>
                 </div>
-            </main>
+            </div>
         </div>
     )
 }
