15d96a14171617e87903d92fc08b83159df3db0a
StunnedGun <110390660+StunnedGun@users.noreply.github.com>
2023-01-19 09:41:11 +0000

    Delete account.js

--- pages/account.js
+++
@@ -1,9 +0,0 @@
-import { AccountPage } from "../components/AccountPage"
-import { useRouter } from "next/router"
-
-export default function Account() {
-    const router = useRouter()
-    const data = router.query
-
-    return <AccountPage session={data}></AccountPage>
-}
