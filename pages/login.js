ab82fa377d830111a72c9b66b90a588a673dc2a9
jamiestungo <jamiestungo@gmail.com>
2023-01-31 14:00:00 +0000

    Redirect on login causing redirects at all times so I commented it out

--- pages/login.js
+++ pages/login.js
@@ -1,23 +1,29 @@
 import { Auth, ThemeSupa } from "@supabase/auth-ui-react"
-import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react"
+import {
+    useSession,
+    useUser,
+    useSupabaseClient,
+} from "@supabase/auth-helpers-react"
 import Account from "../components/Account"
 import { SideBar } from "../components/SideBar"
 import { AccountPage } from "../components/AccountPage"
 import GigsDisplay from "../components/GigsDisplay"
 import Router from "next/router"
+import { useEffect } from "react"
 
 const Login = () => {
     const session = useSession()
     const supabase = useSupabaseClient()
-
-    if (session) {
+    const user = useUser()
+    /*
+    useEffect(() => {
         Router.push({
             pathname: "/gigs",
             //  query: { example: "something" }, // query is optional
         }).then(() => {
             window.scrollTo(0, 0)
         })
-    }
+    }, [])*/
 
     return (
         <div className="container" style={{ padding: "50px 0 100px 0" }}>
